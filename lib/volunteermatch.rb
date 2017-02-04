77e310b1d6321238ef15afd80f6b482bf03fc904
evanscloud <evanscloud@users.noreply.github.com>
2017-02-04 16:31:48 -0500

    Create initial lib directory and set version to 0.1.0

---
+++ lib/volunteermatch.rb
@@ -0,0 +1,12 @@
+require "volunteermatch/version"
+
+require 'base64'
+require 'digest/sha2'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'ostruct'
+
+module Volunteermatch
+  # code goes here...
+end
---
+++ lib/volunteermatch/version.rb
@@ -0,0 +1,3 @@
+module Volunteermatch
+  VERSION = "0.1.0"
+end
