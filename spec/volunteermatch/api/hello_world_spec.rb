037c5bf66ecdc78bde3aa857c0425260deb36767
evanscloud <evanscloud@users.noreply.github.com>
2017-02-07 21:57:29 -0500

    Add additional test to helloWorld api call

--- spec/volunteermatch/api/hello_world_spec.rb
+++ spec/volunteermatch/api/hello_world_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Volunteermatch::API::HelloWorld do
   subject { Volunteermatch::Client.new('VolunteerMatch','test_key') }
 
   describe "helloWorld api call" do
+    it "should call #test with appropriate arguments" do
+      expect(subject).to receive(:test).with("VolunteerMatch")
+      subject.test("VolunteerMatch")
+    end
+
     it "returns the correct name" do
       url = URI.parse("http://www.volunteermatch.org/api/call?action=helloWorld&query=" + URI.encode({name: "VolunteerMatch"}.to_json))
       stub = stub_get(url).to_return(status: 200, body: fixture("hello_world.json"), headers: { content_type: "application/json" })
