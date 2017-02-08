9e0c3788063406ae5c1357761099120007fd311d
Evan Ng <evan.ng@outlook.com>
2017-02-08 14:03:20 -0500

    Fix error for numberOfResults

--- spec/volunteermatch/api/search_organizations_spec.rb
+++ spec/volunteermatch/api/search_organizations_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe Volunteermatch::API::SearchOrganizations do
     end
 
     it "should call #search_organizations with appropriate arguments" do
-      expect(subject).to receive(:search_organizations).with({location: "94108", nbOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"]})
-      subject.search_organizations(location: "94108", nbOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"])
+      expect(subject).to receive(:search_organizations).with({location: "94108", numberOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"]})
+      subject.search_organizations(location: "94108", numberOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"])
     end
 
     it "returns a list of organizations requested" do
-      url = URI.parse("http://www.volunteermatch.org/api/call?action=searchOrganizations&query=" + URI.encode({ location: "94108", nbOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"] }.to_json))
+      url = URI.parse("http://www.volunteermatch.org/api/call?action=searchOrganizations&query=" + URI.encode({ location: "94108", numberOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"] }.to_json))
       stub = stub_get(url).to_return(status: 200, body: fixture("search_organizations.json"), headers: { content_type: "application/json" })
       response = JSON.parse(stub.response.body)
       expect(response).to have_key("organizations")
@@ -22,21 +22,21 @@ RSpec.describe Volunteermatch::API::SearchOrganizations do
     end
 
     it "returns the correct page number requested" do
-      url = URI.parse("http://www.volunteermatch.org/api/call?action=searchOrganizations&query=" + URI.encode({ location: "94108", nbOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"] }.to_json))
+      url = URI.parse("http://www.volunteermatch.org/api/call?action=searchOrganizations&query=" + URI.encode({ location: "94108", numberOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"] }.to_json))
       stub = stub_get(url).to_return(status: 200, body: fixture("search_organizations.json"), headers: { content_type: "application/json" })
       response = JSON.parse(stub.response.body)
       expect(response["currentPage"]).to eq(3)
     end
 
     it "returns the correct number of results requested" do
-      url = URI.parse("http://www.volunteermatch.org/api/call?action=searchOrganizations&query=" + URI.encode({ location: "94108", nbOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"] }.to_json))
+      url = URI.parse("http://www.volunteermatch.org/api/call?action=searchOrganizations&query=" + URI.encode({ location: "94108", numberOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"] }.to_json))
       stub = stub_get(url).to_return(status: 200, body: fixture("search_organizations.json"), headers: { content_type: "application/json" })
       response = JSON.parse(stub.response.body)
       expect(response["resultsSize"]).to eq(2)
     end
 
     it "returns the correct fields requested" do
-      url = URI.parse("http://www.volunteermatch.org/api/call?action=searchOrganizations&query=" + URI.encode({ location: "94108", nbOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"] }.to_json))
+      url = URI.parse("http://www.volunteermatch.org/api/call?action=searchOrganizations&query=" + URI.encode({ location: "94108", numberOfResults: 10, pageNumber: 3, fieldsToDisplay: ["name", "location"], names: ["red cross"] }.to_json))
       stub = stub_get(url).to_return(status: 200, body: fixture("search_organizations.json"), headers: { content_type: "application/json" })
       response = JSON.parse(stub.response.body)
       expect(response["organizations"].first).to have_key("name")
