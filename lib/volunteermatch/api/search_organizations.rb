b93ec362e741bf8f9998fabeb056ff407a3ac5c9
Evan Ng <evan.ng@outlook.com>
2017-02-08 22:14:14 -0500

    Remove default text to display in parameters

--- lib/volunteermatch/api/search_organizations.rb
+++ lib/volunteermatch/api/search_organizations.rb
@@ -4,7 +4,6 @@ module Volunteermatch
       def search_organizations(parameters = {})
         raise ArgumentError, 'location needs to be defined' if parameters[:location].nil? || parameters[:location].empty?
         parameters[:location] = parameters[:location] || nil
-        parameters[:fieldsToDisplay] = parameters[:fieldsToDisplay => ["name", "location"]]
         call(:searchOrganizations, parameters.to_json)
       end
     end
