ced5d264bf99d9214712e2ebf0595296e7dd0be9
evanscloud <dijkstrascloud@gmail.com>
2017-03-02 23:26:27 -0500

    Fix wrong api call for search_opportunities method

--- lib/volunteermatch/api/search_opportunities.rb
+++ lib/volunteermatch/api/search_opportunities.rb
@@ -4,7 +4,7 @@ module Volunteermatch
       def search_opportunities(parameters = {})
         raise ArgumentError, 'location needs to be defined' if parameters[:location].nil? || parameters[:location].empty?
         parameters[:location] = parameters[:location] || nil
-        call(:searchOrganizations, parameters.to_json)
+        call(:SearchOpportunities, parameters.to_json)
       end
     end
   end
